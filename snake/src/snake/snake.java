7e15e946a5e9c5de7156b06f22a27e3df04ea432
KimWonjong123 <bonk159@gmail.com>
2023-05-30 18:26:22 +0900

    Player 배열 Array List로 변경, 경계면충돌, Self bite 검사 추가

--- snake/src/snake/snake.java
+++ snake/src/snake/snake.java
@@ -1,15 +1,17 @@
 package snake;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.Graphics;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.Font;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.util.ArrayList;
 import java.util.Random;
+
+import static snake.snake.*;
+
+enum GameStatus {
+    NOT_STARTED, RUNNING, GAME_OVER
+}
 
 //snake 본체
 class Player {
@@ -18,22 +20,23 @@ class Player {
     private int position_x;//x위치
     private int position_y;//y위치
 
-    public Player(int x,int y) {
+    public Player(int x, int y) {
         this.position_x = x;
         this.position_y = y;
-    	x_direction = 2;
-    	y_direction = 0;
+        x_direction = 2;
+        y_direction = 0;
     }
 
     public int get_x() {
         return this.position_x;
     }
 
-    public int get_y() {
-        return this.position_y ;
-    }
     public void set_x(int position) {
         this.position_x = position;
+    }
+
+    public int get_y() {
+        return this.position_y;
     }
 
     public void set_y(int position) {
@@ -47,207 +50,250 @@ class Player {
     public void move_y(int num) {
         this.position_y += num;
     }
-    public int get_x_direction(){
-    	return this.x_direction;
-    }
-    public int get_y_direction(){
-    	return this.y_direction;
-    }
-    public void x_change_direction(int a) {
-    	this.x_direction = a;
-    }
-    public void y_change_direction(int a) {
-    	this.y_direction = a;
-    }
-    
-}
-class Food {
-	int x_p = 0;
-	int y_p = 0;
-	boolean foodsetting;
-	Food(){
-		foodsetting = false;
-	}
-	public void set_food(Graphics g) {
-		Random random = new Random();
-		int rand_pos;
-		rand_pos = (int)(random.nextInt(20))*30;
-		this.x_p = rand_pos;
-		rand_pos = (int)(random.nextInt(20))*30;
-		this.y_p = rand_pos;
-		this.foodsetting = true;
-	}
-	public boolean get_foodsetting() {
-		return this.foodsetting;
-	}
-	public void set_foodsetting() {
-		this.foodsetting = false;
-	}
-	public int get_x() {
-		return this.x_p;
-	}
-	public int get_y() {
-		return this.y_p;
-	}
-}
-class inital_move extends Thread{
-	private Player[] p;
-	int eaten;
-	inital_move(Player[] p,int eaten){
-		this.p = p;
-		this.eaten = eaten;
-	}
-	public void run(){
-		try{
-			//while(true){
-				
-				if(this.p[0].get_x_direction() == 2) {
-					this.p[0].move_x(30);
-				}
-				else if(this.p[0].get_x_direction() == 1){
-					this.p[0].move_x(-30);
-				}
-				
-				else if(this.p[0].get_y_direction() == 2) {
-					this.p[0].move_y(30);
 
-				}
-				else if(this.p[0].get_y_direction() == 1){
-					this.p[0].move_y(-30);
-				}
-				Thread.sleep(10000);			
-			//}
-		}
-		catch(InterruptedException e){
-			
-		}
-	}
+    public int get_x_direction() {
+        return this.x_direction;
+    }
+
+    public int get_y_direction() {
+        return this.y_direction;
+    }
+
+    public void x_change_direction(int a) {
+        this.x_direction = a;
+    }
+
+    public void y_change_direction(int a) {
+        this.y_direction = a;
+    }
+
 }
+
+class Food {
+    int x_p = 0;
+    int y_p = 0;
+    boolean foodsetting;
+
+    Food() {
+        foodsetting = false;
+    }
+
+    public void set_food(Graphics g) {
+        Random random = new Random();
+        int rand_pos;
+        rand_pos = random.nextInt(snake.WIDTH) * SIZE;
+        this.x_p = rand_pos;
+        rand_pos = random.nextInt(snake.HEIGHT) * SIZE;
+        this.y_p = rand_pos;
+        this.foodsetting = true;
+    }
+
+    public boolean get_foodsetting() {
+        return this.foodsetting;
+    }
+
+    public void set_foodsetting() {
+        this.foodsetting = false;
+    }
+
+    public int get_x() {
+        return this.x_p;
+    }
+
+    public int get_y() {
+        return this.y_p;
+    }
+}
+
+class initial_move extends Thread {
+    int eaten;
+    private final ArrayList<Player> p;
+
+    initial_move(ArrayList<Player> p, int eaten) {
+        this.p = p;
+        this.eaten = eaten;
+    }
+
+    public void run() {
+        if (this.p.get(0).get_x_direction() == 2) {
+            this.p.get(0).move_x(SIZE);
+        } else if (this.p.get(0).get_x_direction() == 1) {
+            this.p.get(0).move_x(-SIZE);
+        } else if (this.p.get(0).get_y_direction() == 2) {
+            this.p.get(0).move_y(SIZE);
+        } else if (this.p.get(0).get_y_direction() == 1) {
+            this.p.get(0).move_y(-SIZE);
+        }
+    }
+}
+
 class game extends JPanel {
-    public Player[] playerobj;
-    private boolean Start_screen;//시작 화면인지 확인
+    //public Player[] playerobj;
+    public ArrayList<Player> playerobj;
     public int eaten;
     Food food;
+    private GameStatus state;//시작 화면인지 확인
+
     game() {
-    	Start_screen = true;
-    	
-    	
-        setPreferredSize(new Dimension(700,700));
+        state = GameStatus.NOT_STARTED;
+
+        setPreferredSize(new Dimension(900, 700));
         setBackground(new Color(255, 255, 255));
         setFocusable(true);
         addKeyListener(new KeyInput());
-        playerobj = new Player[30];
-        for(int i=0;i<30;i++) {
-        	playerobj[i] = new Player(360-30*i,360);
+        playerobj = new ArrayList<Player>();
+        for(int i = 0; i < 4; i++){
+            playerobj.add(new Player((snake.WIDTH * SIZE) / 2 - SIZE * i, (snake.HEIGHT * SIZE) / 2));
         }
         eaten = 4;
         food = new Food();
     }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         running(g);
+		Toolkit.getDefaultToolkit().sync();
         g.setColor(Color.BLACK);
     }
+
+    private void running(Graphics g) {
+        //폰트 지정
+        Font font = new Font("Arial", Font.BOLD, 30);
+        g.setFont(font);
+
+        //시작화면인지 체크
+        if (state == GameStatus.NOT_STARTED) {
+            g.drawString("SNAKE GAME", 225, 200);
+            g.drawString("Press  Enter  to  begin", 150, 400);
+        }
+        //게임 진행중
+        else if(state == GameStatus.RUNNING){
+            g.drawString(Integer.toString(eaten - 4), 10, 30);
+            Thread R1 = new Thread(new initial_move(playerobj, eaten));
+            R1.start();
+            repaint();
+            for (int i = eaten - 1; i > 0; i--) {
+                playerobj.get(i).set_x(playerobj.get(i - 1).get_x());
+                playerobj.get(i).set_y(playerobj.get(i - 1).get_y());
+            }
+            //충돌확인
+            if (collision_food(food, playerobj.get(0))) {
+                eaten++;
+                if (eaten > 30) {
+                    eaten = 30;//최대점수
+                }
+                else {
+                    playerobj.add(new Player(playerobj.get(eaten - 2).get_x(), playerobj.get(eaten - 2).get_y()));
+                }
+                food.set_foodsetting();
+            }
+            if (!food.get_foodsetting()) {
+                food.set_food(g);
+            }
+
+            g.drawRect(0,0,snake.WIDTH * SIZE, snake.HEIGHT * SIZE);
+
+            g.setColor(Color.BLUE);
+            for (int i = 0; i < eaten; i++) {
+                g.fillRect(playerobj.get(i).get_x(), playerobj.get(i).get_y(), SIZE, SIZE);
+            }
+
+            g.setColor(Color.RED);
+            g.fillOval(food.get_x(), food.get_y(), SIZE, SIZE);
+
+            try {
+                Thread.sleep(75);
+
+            } catch (InterruptedException e) {
+                System.out.println("error");
+            }
+            checkGameOver();
+        }
+        else if(state == GameStatus.GAME_OVER) {
+            g.drawString("GAME OVER", 225, 200);
+            g.drawString("Press  Enter  to  Play Again", 150, 400);
+        }
+    }
+
+    private boolean collision_food(Food f, Player p) {
+        return f.get_x() == p.get_x() && f.get_y() == p.get_y();
+    }
+
+    private void checkGameOver() {
+        Player head = playerobj.get(0);
+        System.out.println("X : " + playerobj.get(0).get_x() + " Y : " + playerobj.get(0).get_y());
+        System.out.println("X : " + playerobj.get(1).get_x() + " Y : " + playerobj.get(1).get_y());
+        System.out.println("X : " + playerobj.get(2).get_x() + " Y : " + playerobj.get(2).get_y());
+        boolean boundaryCheck = head.get_x() < 0 ||
+                head.get_x() > (SIZE * snake.WIDTH) ||
+                head.get_y() < 0 ||
+                head.get_y() > (SIZE * snake.HEIGHT);
+        boolean ateItself = false;
+        for (Player p : playerobj.subList(1, eaten)) {
+            if (head.get_x() == p.get_x() && head.get_y() == p.get_y()) {
+                ateItself = true;
+                break;
+            }
+        }
+        System.out.println("boundaryCheck : " + boundaryCheck + " ateItself : " + ateItself);
+        if (boundaryCheck || ateItself) {
+            state = GameStatus.GAME_OVER;
+        }
+    }
+
     //key 입력 받기
     class KeyInput extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
-            if (key == KeyEvent.VK_LEFT&&playerobj[0].get_x_direction() != 2) {
-            	playerobj[0].x_change_direction(1);
-            	playerobj[0].y_change_direction(0);
+            if(state == GameStatus.RUNNING) {
+                if (key == KeyEvent.VK_LEFT && playerobj.get(0).get_x_direction() != 2) {
+                    playerobj.get(0).x_change_direction(1);
+                    playerobj.get(0).y_change_direction(0);
+                }
+                if (key == KeyEvent.VK_RIGHT && playerobj.get(0).get_x_direction() != 1) {
+                    playerobj.get(0).x_change_direction(2);
+                    playerobj.get(0).y_change_direction(0);
+                }
+                if (key == KeyEvent.VK_UP && playerobj.get(0).get_y_direction() != 2) {
+                    playerobj.get(0).y_change_direction(1);
+                    playerobj.get(0).x_change_direction(0);
+                }
+                if (key == KeyEvent.VK_DOWN && playerobj.get(0).get_y_direction() != 1) {
+                    playerobj.get(0).y_change_direction(2);
+                    playerobj.get(0).x_change_direction(0);
+                }
             }
-            if (key == KeyEvent.VK_RIGHT&&playerobj[0].get_x_direction() != 1) {
-            	playerobj[0].x_change_direction(2);
-            	playerobj[0].y_change_direction(0);
+            if (state == GameStatus.NOT_STARTED && key == KeyEvent.VK_ENTER) {
+                state = GameStatus.RUNNING;
             }
-            if (key == KeyEvent.VK_UP&&playerobj[0].get_y_direction() != 2) {
-            	playerobj[0].y_change_direction(1);
-            	playerobj[0].x_change_direction(0);
+            if (state == GameStatus.GAME_OVER && key == KeyEvent.VK_ENTER) {
+                playerobj = new ArrayList<Player>();
+                for(int i = 0; i < 4; i++){
+                    playerobj.add(new Player((snake.WIDTH * SIZE) / 2 - SIZE * i, (snake.HEIGHT * SIZE) / 2));
+                }
+                eaten = 4;
+                food = new Food();
+                state = GameStatus.NOT_STARTED;
             }
-            if (key == KeyEvent.VK_DOWN&&playerobj[0].get_y_direction() != 1) {
-            	playerobj[0].y_change_direction(2);
-            	playerobj[0].x_change_direction(0);
-            }
-            if (key == KeyEvent.VK_ENTER) {
-            	Start_screen = false;
-            }
-            
+
             repaint();
         }
     }
-
-    private void running(Graphics g) {
-    	//폰트 지정
-    	Font font= new Font("Arial",Font.BOLD,30);
-    	g.setFont(font);
-    		
-    	//시작화면인지 체크
-    	if(Start_screen == true) {
-    		g.drawString("SNAKE GAME",225 ,200);
-    		g.drawString("Press  any  key  to  begin", 150, 400);
-    		
-    	}
-    	//게임 진행중
-    	else {
-    		g.drawString(Integer.toString(eaten-4),10 ,30);
-            Thread R1 =new Thread(new inital_move(playerobj,eaten));
-            R1.start();
-			repaint();
-			for(int i=29;i>0;i--) {
-				playerobj[i].set_x(playerobj[i-1].get_x());
-				playerobj[i].set_y(playerobj[i-1].get_y());
-
-			}
-			//충돌확인
-			if(collision_food(food,playerobj[0])==true){
-				eaten += 1;
-				if(eaten>30) {
-					eaten = 30;//최대점수
-				}
-				food.set_foodsetting();
-			}
-        	if(food.get_foodsetting() == false) {
-        		food.set_food(g);
-        	}
-        	
-			for(int i=0;i<eaten;i++) {
-				g.fillRect(playerobj[i].get_x(), playerobj[i].get_y(), 30, 30);
-			}
-			g.fillOval(food.get_x(), food.get_y(),30,30);
-			
-			try {
-				Thread.sleep(50);
-	            
-			} catch (InterruptedException e) {
-	        
-				System.out.println("error");
-	            
-			}
-			
-    	}
-
-    }
-    private boolean collision_food(Food f, Player p) {
-    	if(f.get_x() == p.get_x()) {
-    		if(f.get_y() == p.get_y()) {
-    			return true;
-        	}
-    	}
-    	
-    	return false;
-    }
 }
 
-public class snake{
+public class snake {
+    static final int SIZE = 20;
+    static final int WIDTH = 30;
+    static final int HEIGHT = 30;
     public static void main(String[] args) {
-    	init();
-    	
+        init();
+
     }
 
     static void init() {
-    	//기초 설정
+        //기초 설정
         JFrame frame = new JFrame("Snake Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
@@ -258,7 +304,7 @@ public class snake{
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        
+
 
     }
 }
